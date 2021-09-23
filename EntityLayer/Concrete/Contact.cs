e9aee646c2418fb62770c8f669e2eca41de1a1f3
İlhan Murat ÜLGER <murat.ulger@yandex.com>
2021-09-23 15:09:31 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+        [StringLength(50)]
+        public string ContactUserName { get; set; }
+        [StringLength(50)]
+        public string ContactUserMail { get; set; }
+        [StringLength(50)]
+        public string ContactSubject { get; set; }
+
+        public string ContactMessage { get; set; }
+
+    }
+}
---
+++ MVCKamp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCKamp.MvcApplication" Language="C#" %>
