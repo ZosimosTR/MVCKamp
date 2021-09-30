664c48c783765841908fa3da7e449c1f408ad9c9
İlhan Murat ÜLGER <murat.ulger@yandex.com>
2021-09-30 14:56:45 +0300

    2.Sertifika için Hazır

--- MVCKamp/Controllers/IstatistikController.cs
+++ MVCKamp/Controllers/IstatistikController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFreamework;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,20 @@ namespace MVCKamp.Controllers
 {
     public class IstatistikController : Controller
     {
-        CategoryManager cm = new CategoryManager(new EFCategoryDAL());
+        Context db = new Context();
+       
+
         // GET: Istatistik
         public ActionResult Index()
         {
-            ViewBag.TopKategoriSayisi = cm.GetList().Count().ToString();
+            ViewBag.TopKategoriSayisi = db.Categories.Count();
+            ViewBag.AolanYazarlar = db.Writers.Count(x => x.WriterName.Contains("a"));
+            ViewBag.yazilimBasliklari = db.Headings.Count(x => x.Category.CategoryId == 13);
+            ViewBag.EnFazlaBaslik = db.Headings.Max(x => x.Category.CategoryName);
+            ViewBag.TrueOlanlar = db.Categories.Count(x => x.CategoryStatus == true);
+            ViewBag.FalseOlanlar = db.Categories.Count(x => x.CategoryStatus == false);
+
+
             return View();
   
         }
